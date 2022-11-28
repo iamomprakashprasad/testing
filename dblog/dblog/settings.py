a08f28d968865e335f0c5a540288494b32d0540a
om <om1504.iimt@outlook.com>
2022-11-28 12:03:44 +0530

    connect to psql established

--- dblog/dblog/settings.py
+++ dblog/dblog/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'dblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'admin',
+       'USER': 'admin',
+       'PASSWORD': '123',
+       'HOST': 'localhost',
+       'PORT': '',
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
