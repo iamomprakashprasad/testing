f54d9d03fb74f6f801f4b3fb67c31ca749b4a2ca
om <om1504.iimt@outlook.com>
2022-11-28 22:39:27 +0530

    inluded templates and static files build navbar, footer, basic and index html

--- dblog/app/views.py
+++ dblog/app/views.py
@@ -1,9 +1,10 @@
 """basic view for app"""
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
     """
     index file
     """
-
-    return HttpResponse("hola")
+    index_template=loader.get_template('index.html')
+    return HttpResponse(index_template.render())
