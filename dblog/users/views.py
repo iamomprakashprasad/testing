afd71fd43a5dc982a4a896ddefd1ea7883b4970c
om <om1504.iimt@outlook.com>
2022-12-04 18:47:17 +0530

    user impletented with redirected to its own url

--- dblog/users/views.py
+++ dblog/users/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import redirect
 
 # Create your views here.
 
 def dashboard(request,username=""):
-    index_template=loader.get_template('users/dashboard.html')
     # print(index_template.render())
-    user_data={"username":username}
+    
     print("request --> ",request)
-    print("username --> ",username)
-    return HttpResponse(index_template.render(user_data))    
+    print("username --> ",request.user)
+    if request.user and not username:
+        return redirect(f'/users/dashboard/{request.user}')
+    else:
+        user_data={"user":request.user}
+        index_template=loader.get_template('users/dashboard.html')
+        return HttpResponse(index_template.render(user_data))    
